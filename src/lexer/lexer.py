5ec6b664dd038feed50e596d4b84131781971afc
MelissaAyllon <mayllon@espol.edu.ec>
2025-06-13 22:13:53 -0500

    add: delimiters & literals

--- src/lexer/lexer.py
+++ src/lexer/lexer.py
@@ -2,16 +2,50 @@ import ply.lex as lex
 from reserved import reserved
 from logger import create_log_file
 
-tokens = ['ID'] + list(reserved.values())
+tokens = ['NEWLINE', 'ID', 'NULLABLE', 'LPAREN', 'RPAREN', 'RBRACKET', 'LBRACKET', 'INDENT', 'INDENT_OUT'] + list(reserved.values())
+
+# CONTRIBUCION: MELISSA AYLLON
+def t_NEWLINE(t):
+    r'\n+'
+    t.type = 'NEWLINE'
+    t.lexer.lineno += len(t.value)
+    return t
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = reserved.get(t.value,'ID')    # Check for reserved words
     return t
 
+def t_NULLABLE(t):
+    r'\?'
+    t.type = 'NULLABLE'
+    return t
 
-# Define a rule for ignored characters (whitespace, newlines)
-t_ignore = ' \t\n'
+def t_LPAREN(t):
+    r'\('
+    t.type = 'LPAREN'
+    return t
+
+def t_RPAREN(t):
+    r'\)'
+    t.type = 'RPAREN'
+    return t
+
+def t_LBRACKET(t):
+    r'\['
+    t.type = 'LBRACKET'
+    return t
+
+def t_RBRACKET(t):
+    r'\]'
+    t.type = 'RBRACKET'
+    return t
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.type = 'ILLEGAL'
+    t.lexer.skip(1)
+    return "'%s'" % t.value[0] + " (illegal character)"
 
 
 def get_tokens(data):
@@ -32,7 +66,11 @@ def get_tokens(data):
 
 # Test it out
 data = '''
-function
+function {} [] testFunction()
+hola
+    melissa here
+  D
+OUT
 '''
 # Get tokens from lexico.py
 tokens_list = get_tokens(data)
